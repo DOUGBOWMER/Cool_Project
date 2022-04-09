b94a054d5697be852bf17a58bc94b8a9dab3f30f
DOUGBOWMER <89964268+DOUGBOWMER@users.noreply.github.com>
2022-04-09 15:14:06 -0400

    Delete old secrets

--- next.config.js
+++ next.config.js
@@ -1,25 +1,8 @@
 module.exports = {
   env: {
-    AWS_ACCESS_KEY_ID: 'AKIAUNDZSP6DSYHH45FX',
-    REGION: 'us-east-2',
-    BUCKETNAME: 'yakachat',
-    AWS_SECRET_ACCESS_KEY: '0TRrTOVFFaSmGX3+mGvBinmTrVl7FzKtxB7bOuII',
-    FACEBOOK_CLIENT_ID: "595544941713295",
-    FACEBOOK_CLIENT_SECRET: "d083fb095f10911495fb05745f31907d",
     NEXTAUTH_URL: "http://localhost:3000",
-    GOOGLE_CLIENT_ID: '',
-    GOOGLE_CLIENT_SECRET: '',
     SECRET: 'vroomvroom',
-    UploadBucket: "yakachat",
     
-    FirestoreCreditials:"",
-    apiKey: "AIzaSyDo1ohiXMp5RzoxU43ptyEXkBLdA5MZSNk",
-    authDomain: "dev-folio-2d03a.firebaseapp.com",
-    projectId: "dev-folio-2d03a",
-    storageBucket: "dev-folio-2d03a.appspot.com",
-    messagingSenderId: "526001926002",
-    appId: "1:526001926002:web:23819f254df315f79277eb",
-    measurementId: "G-3J9QXP268F"
 
   },
   images: {
